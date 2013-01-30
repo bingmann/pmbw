d8ffc5f95c55553b0f5350c9246bc6d034271878
Timo Bingmann <tbgit@panthema.net>
2013-01-30 18:48:33 +0100

    Fixing race condition in main loop, which lead to endless runs. Depending on ts2-ts1 < 1.0, the areasize iterator was adavanced or not.

--- main.cc
+++ main.cc
@@ -214,49 +214,48 @@ void testfunc( char* memarea, const size_t memsize,
             continue;
         }
 
-        size_t factor = 1024*1024*1024;             // repeat factor, approximate B/s bandwidth
+        size_t factor = 1024*1024*1024;         // repeat factor, approximate B/s bandwidth
+
+        for (const size_t* areasize = areasize_list; *areasize; ++areasize)
+        {
+            if (*areasize > g_sizelimit && g_sizelimit != 0) {
+                ERR("Skipping " << funcname << " test with " << *areasize << " array size due to -s <size limit>.");
+                continue;
+            }
+
+            size_t thrsize = *areasize / nprocs;            // divide area by processor number
+
+            // unrolled tests do 16 accesses without loop check, thus align upward 
+            // to next multiple of 16*size (128 bytes for 64-bit and 256 bytes for 128-bits)
+            size_t unrollsize = 16 * access_size;
+            thrsize = ((thrsize + unrollsize) / unrollsize) * unrollsize;
+
+            size_t testsize = thrsize * nprocs;             // total size tested
+            if (memsize < testsize) continue;               // skip if tests don't fit into memory
+
+            // due to cache thrashing, space out processor's test areas
+            size_t thrsize_spaced = std::max<size_t>(thrsize, 32*1024*1024);
+            if (memsize < thrsize_spaced * nprocs) continue;        // skip if tests don't fit into memory
+
+            size_t repeats = (factor + thrsize-1) / thrsize;         // round up
+
+            size_t testvol = testsize * repeats * access_size / skiplen;        // volume in bytes tested
+            size_t testaccess = testsize * repeats / skiplen;                   // number of accesses in test
+
+            ERR("Running"
+                << " nprocs=" << nprocs
+                << " factor=" << factor
+                << " areasize=" << *areasize
+                << " thrsize=" << thrsize
+                << " testsize=" << testsize
+                << " repeats=" << repeats
+                << " testvol=" << testvol
+                << " testaccess=" << testaccess);
+
+            double runtime;
 
 #pragma omp parallel num_threads(nprocs)
-        {
-            for (const size_t* p_areasize = areasize_list; *p_areasize; ++p_areasize)
             {
-                const size_t& areasize = *p_areasize;
-
-                size_t thrsize = areasize / nprocs;             // divide area by processor number
-
-                // unrolled tests do 16 accesses without loop check, thus align upward 
-                // to next multiple of 16*size (128 bytes for 64-bit and 256 bytes for 128-bits)
-                size_t unrollsize = 16 * access_size;
-                thrsize = ((thrsize + unrollsize) / unrollsize) * unrollsize;
-
-                size_t testsize = thrsize * nprocs;             // total size tested
-                if (memsize < testsize) continue;              // skip if tests don't fit into memory
-
-                // due to cache thrashing, space out processor's test areas
-                size_t thrsize_spaced = std::max<size_t>(thrsize, 32*1024*1024);
-                if (memsize < thrsize_spaced * nprocs) continue;        // skip if tests don't fit into memory
-
-                size_t repeats = (factor + thrsize-1) / thrsize;         // round up
-
-                size_t testvol = testsize * repeats * access_size / skiplen;        // volume in bytes tested
-                size_t testaccess = testsize * repeats / skiplen;                   // number of accesses in test
-
-                if (areasize > g_sizelimit && g_sizelimit != 0) {
-                    ERR("Skipping " << funcname << " test with " << areasize << " array size due to -s <size limit>.");
-                    continue;
-                }
-
-#pragma omp single
-                ERR("Running"
-                    << " nprocs=" << nprocs
-                    << " factor=" << factor
-                    << " areasize=" << areasize
-                    << " thrsize=" << thrsize
-                    << " testsize=" << testsize
-                    << " repeats=" << repeats
-                    << " testvol=" << testvol
-                    << " testaccess=" << testaccess);
-
                 // create cyclic permutation for each processor
                 if (use_permutation)
                     make_cyclic_permutation(memarea + omp_get_thread_num() * thrsize_spaced, thrsize);
@@ -267,61 +266,55 @@ void testfunc( char* memarea, const size_t memsize,
 
 #pragma omp barrier
                 double ts2 = omp_get_wtime();
+#pragma omp master
+                runtime = ts2 - ts1;
+            }
 
-                if ( ts2-ts1 < 1.0 )
-                {
-#pragma omp single
-                    {
-                        // test ran for less than one second, repeat test and scale repeat factor
-                        factor = thrsize * repeats * 3/2 / (ts2-ts1);
-                        ERR("run time = " << (ts2-ts1) << " -> rerunning test with repeat factor=" << factor);
-                    }
+            if ( runtime < 1.0 )
+            {
+                // test ran for less than one second, repeat test and scale repeat factor
+                factor = thrsize * repeats * 3/2 / runtime;
+                ERR("run time = " << runtime << " -> rerunning test with repeat factor=" << factor);
 
-                    --p_areasize;
-                    continue;
-                }
-
+                --areasize;     // redo this areasize
+            }
+            else
+            {
                 // adapt repeat factor to observed memory bandwidth, so that next test will take approximately 1.5 sec
-#pragma omp single
-                {
-                    factor = thrsize * repeats * 3/2 / (ts2-ts1);
-                    ERR("run time = " << (ts2-ts1) << " -> next test with repeat factor=" << factor);
-                }
 
-#pragma omp single
-                {
-                    std::ostringstream resultline;
+                factor = thrsize * repeats * 3/2 / runtime;
+                ERR("run time = " << runtime << " -> next test with repeat factor=" << factor);
 
-                    resultline << "RESULT\t";
+                std::ostringstream result;
+                result << "RESULT\t";
 
-                    // output date, time and hostname to resultline
-                    char datetime[64];
-                    time_t tnow = time(NULL);
+                // output date, time and hostname to result line
+                char datetime[64];
+                time_t tnow = time(NULL);
 
-                    strftime(datetime, sizeof(datetime), "%Y-%m-%d %H:%M:%S", localtime(&tnow));
-                    resultline << "datetime=" << datetime << "\t";
+                strftime(datetime, sizeof(datetime), "%Y-%m-%d %H:%M:%S", localtime(&tnow));
+                result << "datetime=" << datetime << "\t";
 
-                    char hostname[256];
-                    gethostname(hostname, sizeof(hostname));
-                    resultline << "host=" << hostname << "\t";
+                char hostname[256];
+                gethostname(hostname, sizeof(hostname));
+                result << "host=" << hostname << "\t";
 
-                    resultline << "funcname=" << funcname << "\t"
-                               << "nprocs=" << nprocs << "\t"
-                               << "areasize=" << areasize << "\t"
-                               << "threadsize=" << thrsize << "\t"
-                               << "testsize=" << testsize << "\t"
-                               << "repeats=" << repeats << "\t"
-                               << "testvol=" << testvol << "\t"
-                               << "testaccess=" << testaccess << "\t"
-                               << "time=" << std::setprecision(20) << (ts2-ts1) << "\t"
-                               << "bandwidth=" << testvol / (ts2-ts1) << "\t"
-                               << "rate=" << (ts2-ts1) / testaccess;
+                result << "funcname=" << funcname << "\t"
+                           << "nprocs=" << nprocs << "\t"
+                           << "areasize=" << *areasize << "\t"
+                           << "threadsize=" << thrsize << "\t"
+                           << "testsize=" << testsize << "\t"
+                           << "repeats=" << repeats << "\t"
+                           << "testvol=" << testvol << "\t"
+                           << "testaccess=" << testaccess << "\t"
+                           << "time=" << std::setprecision(20) << runtime << "\t"
+                           << "bandwidth=" << testvol / runtime << "\t"
+                           << "rate=" << runtime / testaccess;
 
-                    std::cout << resultline.str() << std::endl;
+                std::cout << result.str() << std::endl;
 
-                    std::ofstream resultfile("stats.txt", std::ios::app);
-                    resultfile << resultline.str() << std::endl;
-                }
+                std::ofstream resultfile("stats.txt", std::ios::app);
+                resultfile << result.str() << std::endl;
             }
         }
     }
@@ -398,7 +391,7 @@ int main(int argc, char* argv[])
     size_t memsize = round_up_power2(physical_mem) / 2;
 
     // due to roundup in loop to next cache-line size, add one extra cache-line per processor
-    memsize += omp_get_num_procs() * 128;
+    memsize += omp_get_num_procs() * 256;
 
     ERR("Detected " << physical_mem / 1024/1024 << " MiB physical RAM and " << omp_get_num_procs() << " CPUs. " << std::endl
         << "Allocating " << memsize / 1024/1024 << " MiB for testing.");
